30c163f3536de000b0d2e42ad3f268783de89416
alexmartinez8295 <alexismh8295@gmail.com>
2026-06-05 14:14:34 -0600

    Cambio en texto de pestaña

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Background from "@/components/Background";
+
+export const metadata: Metadata = {
+  title: "AlxDev!",
+};
 
 export default function RootLayout({
   children,
